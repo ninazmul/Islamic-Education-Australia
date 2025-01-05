93ec76cbcb80e592e1dcd5029ae1f8a47edab90d
ninazmul <nazmulsaw@gmail.com>
2025-01-05 11:23:08 +0600

    profile page updated

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,5 +1,4 @@
 import RegistrationFrom from "@/app/dashboard/components/RegistrationForm";
-import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -9,26 +8,18 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { getOrdersByUser } from "@/lib/actions/order.actions";
 import {
   getRegistrationsByUserId,
   isRegistered,
   isSubmitted,
 } from "@/lib/actions/registration.actions";
 import { getUserById } from "@/lib/actions/user.actions";
-import { IOrder } from "@/lib/database/models/order.model";
 import { auth } from "@clerk/nextjs/server";
 import { Clock, Edit, Eye, Plus } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-type PageProps = {
-  searchParams: Promise<{
-    [key: string]: string | string[] | undefined;
-  }>;
-};
-
-const ProfilePage = async ({ searchParams }: PageProps) => {
+const ProfilePage = async ( ) => {
   const { sessionClaims } = await auth();
   const userId = sessionClaims?.userId as string;
   const user = await getUserById(userId);
@@ -38,12 +29,6 @@ const ProfilePage = async ({ searchParams }: PageProps) => {
   const isSubmittedUser = await isSubmitted(userId);
   const isRegisteredUser = await isRegistered(userId);
 
-  const resolvedSearchParams = await searchParams;
-  const ordersPage = Number(resolvedSearchParams?.ordersPage) || 1;
-
-  const orders = await getOrdersByUser({ userId, page: ordersPage });
-
-  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
 
   return (
     <>
@@ -161,28 +146,6 @@ const ProfilePage = async ({ searchParams }: PageProps) => {
             </div>
           </div>
         </div>
-      </section>
-      {/* My Tickets */}
-      <section className="bg-green-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/events">Explore More Events</Link>
-          </Button>
-        </div>
-      </section>
-
-      <section className="wrapper my-8">
-        <Collection
-          data={orderedEvents}
-          emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="No worries - plenty of exciting events to explore!"
-          collectionType="My_Tickets"
-          limit={3}
-          page={ordersPage}
-          urlParamName="ordersPage"
-          totalPages={orders?.totalPages}
-        />
       </section>
     </>
   );
