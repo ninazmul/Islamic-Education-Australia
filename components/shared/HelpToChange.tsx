b7db0c5fc3416940dd48562fd5c0f17294bfda09
ninazmul <nazmulsaw@gmail.com>
2025-01-06 16:54:57 +0600

    help to change md design done

--- components/shared/HelpToChange.tsx
+++ components/shared/HelpToChange.tsx
@@ -32,18 +32,20 @@ const HelpToChange = () => {
             </Button>
           </div>
         </div>
-        <div className="flex justify-center w-full lg:w-1/2">
+        <div className="flex justify-center w-full lg:w-1/2 relative">
           <Image
             src="/assets/images/Service/knowledge.png"
             width={500}
             height={500}
             alt="Islamic Education Australia Ltd logo"
-            className="w-2/3"
+            className="w-2/3 z-10"
           />
+          <div className="absolute left-[65%] md:-top-[13%] lg:-top-[20%] rounded-none p-20 bg-yellow-400 " />
+          <div className="absolute md:left-[11.5%] lg:left-[8%] md:-bottom-[12%] lg:-bottom-[20%] rounded-none p-20 bg-yellow-400 " />
         </div>
       </div>
-      <div className="absolute left-[80%] top-[15%] rounded-none p-20 bg-primary-500 lg:bg-yellow-400 " />
-      <div className="absolute right-[35.5%] bottom-[20%] rounded-none p-20 bg-primary-500 lg:bg-yellow-400 " />
+      {/* <div className="absolute left-[80%] top-[15%] rounded-none p-20 bg-primary-500 lg:bg-yellow-400 " />
+      <div className="absolute right-[35.5%] bottom-[20%] rounded-none p-20 bg-primary-500 lg:bg-yellow-400 " /> */}
     </div>
   );
 };
